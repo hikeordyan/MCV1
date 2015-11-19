caa46c4f4c7c41d2e12cff0407dbf2c4529b9cfd
hayk.ordyan <hike.ordyan@gmail.com>
2015-11-19 11:36:43 +0400

    mvc

---
+++ MvcApplication1/Controllers/TestController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+namespace MvcApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+         [HttpPost]
+        public ActionResult Data()
+        {
+            /*var data = new Test[9]
+            {
+              new Test(1,"hayk","Yerevan"),
+              new Test(2,"aram","Gyumry"),
+              new Test(3,"ashot","Idjevan"),
+              new Test(4,"ani","Hrazdan"),
+              new Test(5,"vahe","Kapan"),
+              new Test(6,"shushan","Ararat"),
+              new Test(7,"anna","Vanadzor"),
+              new Test(8,"suren","Yerevan"),
+              new Test(9,"hayk","Yerevan"),
+            };
+
+            return View(data);*/
+            
+            return View();
+        }
+
+
+
+
+        public ActionResult Index()
+        {
+
+            IEnumerable<Test>  data = new Test[9]
+            {
+              new Test(1,"hayk","Yerevan"),
+              new Test(2,"aram","Gyumry"),
+              new Test(3,"ashot","Idjevan"),
+              new Test(4,"ani","Hrazdan"),
+              new Test(5,"vahe","Kapan"),
+              new Test(6,"shushan","Ararat"),
+              new Test(7,"anna","Vanadzor"),
+              new Test(8,"suren","Yerevan"),
+              new Test(9,"hayk","Yerevan"),
+            };
+
+
+           /* var model = Enumerable.Range(1,5)
+               .Select(i => data);*/
+
+            return View(data);
+          
+        }
+
+    }
+}
---
+++ MvcApplication1/Models/Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.Collections;
+namespace MvcApplication1.Models
+{
+    public class Test
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string address { get; set; }
+        
+        public Test(int ID,string Name,string address)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.address = address;
+        }
+
+        public Test()
+        {
+            // TODO: Complete member initialization
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2};", ID, Name , address);
+        }
+
+        
+    }
+
+    
+}
